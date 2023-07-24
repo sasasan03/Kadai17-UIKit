2067ec2572bc9713591cdf55b459c9f62d22bec9
sasasan03 <sako.hiro62@gmail.com>
2023-07-24 19:37:14 +0900

    [add]: TableView・TableViewCell・ItemAddEditViewを追加

---
+++ Kadai17-UIKit/TableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  TableViewCell.swift
+//  Kadai17-UIKit
+//
+//  Created by sako0602 on 2023/07/24.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    static let cellNibName = "TableViewCell"
+    static let cellIdentifier = "Cell"
+    
+    @IBOutlet weak var checkMark: UIImageView!
+    @IBOutlet weak var itemText: UILabel!
+    
+    func configure(item: Item){
+        checkMark.image =  UIImage(systemName: item.isChecked ? "checkmark.diamond.fill" : "checkmark.diamond")
+    }
+   
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+    
+}
--- Kadai17-UIKit/ViewController.swift
+++ Kadai17-UIKit/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+//MARK: 配列の要素を構成するデータモデル型
+struct Item {
+    var name: String
+    var isChecked: Bool
+}
 
 class ViewController: UIViewController {
+    
+    var items = [
+        Item(name: "えんぴつ", isChecked: false),
+        Item(name: "消しゴム", isChecked: true),
+        Item(name: "ものさし", isChecked: false)
+    ]
+    
+    @IBOutlet weak var itemTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        itemTableView.dataSource = self
+        itemTableView.delegate = self
+        itemTableView.register(UINib(nibName: TableViewCell.cellNibName, bundle: nil), forCellReuseIdentifier: TableViewCell.cellIdentifier)
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.cellIdentifier, for: indexPath) as! TableViewCell
+        cell.configure(item: items[indexPath.row])
+        return cell
+    }
+    
+    
+}
