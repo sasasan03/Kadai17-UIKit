7a71d3802d7412b10da4a426163827c5bfbb3311
sasasan03 <sako.hiro62@gmail.com>
2023-07-24 18:24:45 +0900

    first commmit

---
+++ Kadai17-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai17-UIKit
+//
+//  Created by sako0602 on 2023/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
